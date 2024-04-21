82d2c2033b60d78b06c89dff7dd48f5ac06c8e13
ShaneKanterman04 <122749779+ShaneKanterman04@users.noreply.github.com>
2024-04-21 12:13:32 -0400

    changed to correct dataset

--- streams_by_country.py
+++ streams_by_country.py
@@ -2,22 +2,28 @@
 
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def show_page():
     st.title('Streams by Country')
-    df = pd.read_csv('https://raw.githubusercontent.com/ShaneKanterman04/School/main/streams_by_country_2023.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/duducury/spotify-data/main/top5_artists_by_streams.csv')
 
-    # Allow user to select countries for comparison
-    country1 = st.selectbox('Select Country 1', df['country'].unique())
-    country2 = st.selectbox('Select Country 2', df['country'].unique())
+    # Create selectors for country and year
+    selected_country1 = st.selectbox('Select Country 1', df['region'].unique())
+    selected_country2 = st.selectbox('Select Country 2', df['region'].unique())
+    selected_year = st.selectbox('Select Year', df['year'].unique())
 
-    # Filter data for the selected countries
-    df_country1 = df[df['country'] == country1]
-    df_country2 = df[df['country'] == country2]
+    # Filter DataFrame based on selected country and year
+    filtered_df1 = df[(df['region'] == selected_country1) & (df['year'] == selected_year)]
+    filtered_df2 = df[(df['region'] == selected_country2) & (df['year'] == selected_year)]
 
-    # Merge data for both countries
-    df_combined = pd.concat([df_country1.set_index('country'), df_country2.set_index('country')], axis=1)
+    # Calculate total streams for each country
+    total_streams_country1 = filtered_df1['streams'].sum()
+    total_streams_country2 = filtered_df2['streams'].sum()
 
-    # Bar chart to compare streams between the two selected countries
-    st.subheader('Streams Comparison')
-    st.bar_chart(df_combined)
+    # Plot the bar graph
+    fig, ax = plt.subplots()
+    ax.bar([selected_country1, selected_country2], [total_streams_country1, total_streams_country2])
+    ax.set_ylabel('Total Streams')
+    ax.set_title(f'Total Streams Comparison for {selected_year}')
+    st.pyplot(fig)
