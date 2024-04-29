f9dfda08b6d4b8b803c1761f6a2c8c07f40fc0ec
Eduardo Cury <ecury0001@mail.ct.edu>
2024-04-28 22:14:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,14 +46,15 @@ Our dashboard offers a comprehensive analysis of data including:
 # Update the pages dictionary to include the new page
 pages = {
     "Home": home_page,
+    "The Progress of Music":stremingPlatforms.show_page,# Add the new page
     "Top 5 Streamed Artist": top_streams_artist.show_top_artists,
     "Global Trends": global_trends.show_page,
-    "Revenue Analysis": revenue_analysis.show_page,
-    "Streams by Country": streams_by_country.show_page,
-    "Royalities Comparsion":revcomp.show_page,# Add the new page
-    "The Progress of Music":stremingPlatforms.show_page,# Add the new page
+    "Streams by Country": streams_by_country.show_page, 
     "How Covid Impacted Streaming": how_covid_impacts_streaming.show_page,
-}
+    "Royalities Comparsion":revcomp.show_page,# Add the new page 
+    "Revenue Analysis": revenue_analysis.show_page,
+ }   
+   
 
 # Navigation menu
 st.sidebar.title('Navigation')
