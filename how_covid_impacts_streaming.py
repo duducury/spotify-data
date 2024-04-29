28af0f64c90f226999f9fd78dce7c8eb86126087
ShaneKanterman04 <122749779+ShaneKanterman04@users.noreply.github.com>
2024-04-29 15:28:21 -0400

    Update how_covid_impacts_streaming.py

--- how_covid_impacts_streaming.py
+++ how_covid_impacts_streaming.py
@@ -23,7 +23,7 @@ def show_page():
 
     # Analysis
     st.write("## Analysis:")
-    st.write("The data reveals interesting patterns in streaming numbers surrounding the COVID-19 pandemic. Prior to the onset of COVID-19, there was a gradual decline in streaming numbers, indicating a possible trend of saturation or shifting consumer preferences. However, this trend abruptly changed with the emergence of the pandemic.")
+    st.write("The data reveals interesting patterns in streaming numbers surrounding the COVID-19 pandemic. Prior to the onset of COVID-19, there was a gradual decline in streaming numbers, aligning with Spotify increasing their cost of subscription. However, this trend abruptly changed with the emergence of the pandemic.")
     st.write("Once COVID-19 started spreading globally and governments implemented lockdown measures, there was a notable spike in total streams. This surge can be attributed to several factors. Firstly, with people spending more time at home due to lockdowns and social distancing measures, there was an increased demand for entertainment options.")
     st.write("Additionally, the closure of cinemas, theaters, and other entertainment venues likely led individuals to seek alternative forms of entertainment, such as streaming music and movies. Moreover, with remote work and online learning becoming the norm, individuals may have turned to streaming platforms to help alleviate stress and create a sense of normalcy during uncertain times.")
     st.write("Furthermore, the availability of diverse content on streaming platforms, coupled with advancements in technology and streaming services' aggressive marketing strategies, also contributed to the surge in streaming numbers.")
